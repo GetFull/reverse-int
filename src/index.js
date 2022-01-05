325ccb1f02ccae32216590aed1cecdddf6db189a
getfull <gtfullz@gmail.com>
2022-01-05 22:23:08 +0200

    feat:  add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const numberToText = Math.abs(n).toString();
+    const numberLenght = numberToText.length - 1;
+    let result = "";
+
+    for (let i = numberLenght; i >= 0; i--) {
+        result += numberToText[i];
+    }
+
+    return +result;
+};
